a29a6467e91bd8f379d4c2122b3d4c91b3e61789
iBug <git@ibugone.com>
2021-12-05 03:01:29 +0800

    Verify user from cookies

--- main.rb
+++ main.rb
@@ -4,65 +4,104 @@
 require 'base64'
 require 'cgi'
 require 'erb'
-require 'net/http'
-require 'ostruct'
-require 'time'
 require 'json'
+require 'net/http'
+require 'openssl'
+require 'securerandom'
+require 'time'
 require 'yaml'
 
 TEMPLATE_DIR = 'templates'
 CONFIG = YAML.load_file('config.yml')
 
+def hmac_sha1(key, data)
+  digest = OpenSSL::Digest.new('sha1')
+  OpenSSL::HMAC.hexdigest(digest, key, data)
+end
+
 class Template
   def initialize(name = 'base')
-    @template = Template.get(name)
-  end
-
-  def self.get(name)
-    ERB.new(File.read(File.join(TEMPLATE_DIR, "#{name}.erb")))
+    @template = self.class.get(name)
   end
 
   def render
     @template.result(binding)
   end
-end
 
-BASE_TEMPLATE = Template.new
+  class << self
+    def get(name)
+      ERB.new(File.read(File.join(TEMPLATE_DIR, "#{name}.erb")))
+    end
 
-def render(filename)
-  BASE_TEMPLATE.render do
-    Template.get(filename).result(binding)
-  end
-end
-
-class RequestHandler
-  def initialize(url_key, queryParams, headers)
-    @url_key = url_key
-    @queryParams = queryParams
-    @headers = headers
-  end
-
-  def session_info
-    return {} unless @headers['cookie']
-    CGI::Cookie.parse(@headers['cookie'])
-  end
-
-  def handle
-    @session = session_info
-    case @url_key
-    when ''
-      render 'index'
+    def base
+      @base ||= self.new
     end
   end
 end
 
-def entrypoint(event:, context:)
-  key = event['rawPath'].delete_prefix '/'
-  queryParams = event['queryStringParameters'] || {}
-  output, status_code, headers = RequestHandler.new(key, queryParams, event['headers']).handle
+module ERBHelper
+  def h(s)
+    CGI::escapeHTML s
+  end
+end
 
-  status_code ||= 200
-  headers ||= {}
+class RequestHandler
+  extend ERBHelper
+
+  def initialize(req_path, req_params, req_headers)
+    @req_path = req_path
+    @req_params = req_params
+    @req_headers = req_headers
+
+    @res_body = ''
+    @res_code = 200
+    @res_headers = { 'Content-Type' => 'text/plain' }
+  end
+
+  def render(template)
+    @res_body = Template.base.render do
+      Template.get(template).result(binding)
+    end
+    @res_headers['Content-Type'] = 'text/html'
+  end
+
+  def cookies
+    @cookies ||= CGI::Cookie.parse(@req_headers['Cookie'])
+  end
+
+  def id_from_cookie(cookie_name)
+    id, timestamp, sig = cookies[cookie_name].split('+')
+    hmac = hmac_sha1(CONFIG['session_key'], "#{id}+#{timestamp}")
+    fail unless hmac == sig
+    fail unless Time.now.to_i < timestamp.to_i + CONFIG['session_expiry']
+    id
+  rescue
+  end
+
+  def user_ustc
+    @user_ustc ||= id_from_cookie 'user_ustc'
+  end
+
+  def user_github
+    @user_github ||= id_from_cookie 'user_github'
+  end
+
+  def handle
+    case @req_path
+    when '/'
+      render 'index'
+    end
+
+    [@res_body, @res_code, @res_headers]
+  end
+end
+
+# For AWS Lambda invocation
+def entrypoint(event:, context:)
+  url_path = event['rawPath']
+  query_params = event['queryStringParameters'] || {}
+  output, status_code, headers = RequestHandler.new(url_path, query_params, event['headers']).handle
+
   headers['Content-Type'] ||= 'text/html'
   { statusCode: status_code, headers: headers, body: output }
 rescue StandardError => e
@@ -70,6 +109,7 @@ rescue StandardError => e
   { statusCode: 500, headers: { 'Content-Type' => 'text/plain' }, body: "Internal Server Error\n" }
 end
 
+# For command-line invocation
 def main
   warn "Invocation URL: #{ARGV[0]}"
   arg_uri = URI(ARGV[0].to_s)
