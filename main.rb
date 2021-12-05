a41c556d1446a69963abb7f5dab8a6e42723c6b8
iBug <git@ibugone.com>
2021-12-05 16:38:17 +0800

    Add token validation page

--- main.rb
+++ main.rb
@@ -6,7 +6,6 @@ require 'cgi'
 require 'erb'
 require 'json'
 require 'net/http'
-require 'nokogiri'
 require 'openssl'
 require 'securerandom'
 require 'time'
@@ -18,10 +17,6 @@ CONFIG = YAML.load_file('config.yml')
 def hmac_sha1(key, data)
   digest = OpenSSL::Digest.new('sha1')
   OpenSSL::HMAC.hexdigest(digest, key, data)
-end
-
-def admins
-  CONFIG['admins']
 end
 
 class Template
@@ -44,23 +39,26 @@ class Template
   end
 end
 
-module ERBHelper
-  def h(s)
-    CGI.escapeHTML s
-  end
-end
-
 class RequestHandler
-  extend ERBHelper
+  def initialize(event)
+    @event = event
+    @context = event['requestContext']
 
-  def initialize(req_path, req_params, req_headers)
-    @req_path = req_path
-    @req_params = req_params
-    @req_headers = req_headers
+    @req_method = @context['http']['method'].upcase
+    @req_host = @context['domainName']
+    @req_path = @event['rawPath']
+    @req_params = @event['queryStringParameters'] || {}
+    @req_headers = @event['headers']
+    @req_body = @event['body']
+    @req_body = Base64.decode64(@req_body) if @event['isBase64Encoded']
 
     @res_body = ''
     @res_code = 200
     @res_headers = { 'Content-Type' => 'text/plain' }
+  end
+
+  def h(s)
+    CGI.escapeHTML s
   end
 
   def render(template)
@@ -70,7 +68,7 @@ class RequestHandler
     @res_headers['Content-Type'] = 'text/html'
   end
 
-  def redirect(target, code=302)
+  def redirect(target, code = 302)
     @res_code = code
     @res_headers['Location'] = target
   end
@@ -83,7 +81,7 @@ class RequestHandler
     expires ||= Time.now + CONFIG['session_expiry']
     if value.nil?
       value = ''
-      expires = Time.at(0)
+      expires = Time.at 0
     end
     @res_headers['Set-Cookie'] ||= []
     @res_headers['Set-Cookie'] << CGI::Cookie.new('name' => name, 'value' => value, 'path' => '/', 'expires' => expires).to_s
@@ -91,14 +89,23 @@ class RequestHandler
 
   def id_from_cookie(cookie_name)
     return unless cookies.key? cookie_name
-    id, timestamp, sig = cookies[cookie_name].split('+')
-    hmac = hmac_sha1(CONFIG['session_key'], "#{id}+#{timestamp}")
+
+    id, timestamp, sig = cookies[cookie_name].split(':')
+    hmac = hmac_sha1(CONFIG['session_key'], "#{id}:#{timestamp}")
     raise unless hmac == sig
     raise unless Time.now.to_i < timestamp.to_i + CONFIG['session_expiry']
 
     id
   rescue StandardError => e
     set_cookie cookie_name
+  end
+
+  def id_to_cookie(cookie_name, id)
+    now = Time.now
+    payload = "#{id}:#{now.to_i}"
+    sig = hmac_sha1(CONFIG['session_key'], payload)
+    set_cookie cookie_name, "#{payload}:#{sig}", now + CONFIG['session_expiry']
+    sig
   end
 
   def user_ustc
@@ -109,20 +116,47 @@ class RequestHandler
     @user_github ||= id_from_cookie 'user_github'
   end
 
+  def token
+    return unless user_ustc && user_github
+
+    payload = "#{user_ustc}:#{user_github}"
+    sig = hmac_sha1(CONFIG['token_key'], payload)
+    @token ||= "#{payload}:#{sig}"
+  end
+
+  def validate_token
+    return if @req_body.nil? || @req_body.empty?
+
+    content = URI.decode_www_form(@req_body).to_h
+    @token = content['token']
+    return if @token.nil? || @token.empty?
+    payload, _, sig = @token.rpartition(':')
+    hmac = hmac_sha1(CONFIG['token_key'], payload)
+    @validate_token = hmac == sig
+  rescue StandardError => e
+  end
+
   def handle
     case @req_path
     when '/'
       render 'index'
     when '/robots.txt'
       @res_body = "User-agent: *\nDisallow: /\n"
+
     when '/logout'
-      set_cookie 'user_ustc', '', Time.at(0)
-      set_cookie 'user_github', '', Time.at(0)
+      set_cookie 'user_ustc'
+      set_cookie 'user_github'
       render 'logout'
     when '/logout-ustc'
       redirect CONFIG['cas']['logout']
     when '/logout-github'
       redirect './'
+
+    when '/token'
+      render 'token'
+    when '/validate'
+      validate_token if @req_method == 'POST'
+      render 'validate'
     else
       @res_body = "Not found\n"
       @res_code = 404
@@ -134,14 +168,12 @@ end
 
 # For AWS Lambda invocation
 def entrypoint(event:, context:)
-  url_path = event['rawPath']
-  query_params = event['queryStringParameters'] || {}
-  output, status_code, headers = RequestHandler.new(url_path, query_params, event['headers']).handle
+  output, status_code, headers = RequestHandler.new(event).handle
 
   headers['Content-Type'] ||= 'text/plain'
   mvheaders, headers = headers.partition { |_, v| v.is_a? Array }.map(&:to_h)
   mvheaders.each do |k, v|
-    v.each_with_index do |v_, idx|
+    v.each_with_index do |v_, _idx|
       k_ = k.chars.map { |c| c.send(%i[upcase downcase].sample) }.join
       headers[k_] = v_
     end
@@ -160,7 +192,7 @@ def main
                    'rawPath' => arg_uri.path,
                    'queryStringParameters' => URI.decode_www_form(arg_uri.query || '').to_h,
                    'headers' => {}
-                 }, context: {})
+                 }, context: nil)
   if r[:statusCode] == 200
     $stdout.write r[:body]
   else
